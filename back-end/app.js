8d5ae68f9dad77167d24994b1d5674ce043e1b1b
shugao0624 <gaoshuyang0624@gmail.com>
2021-11-04 12:36:03 -0400

    added all the route handlers for different pages

--- back-end/app.js
+++ back-end/app.js
@@ -1,5 +1,33 @@
 // import and instantiate express
 const express = require("express"); // CommonJS import style!
+const {
+  default: AvAddToQueue,
+} = require("material-ui/svg-icons/av/add-to-queue");
 const app = express(); // instantiate an Express object
 app.use("/static", express.static("public"));
+
+app.get("/home", (req, res) => {});
+
+app.get("/about", (req, res) => {});
+
+app.get("/contact", (req, res) => {});
+
+app.get("/login", (req, res) => {});
+
+app.get("/usersignup", (req, res) => {});
+
+app.get("/userprofile", (req, res) => {});
+
+app.get("/userfollowing", (req, res) => {});
+
+app.get("/searchresults", (req, res) => {});
+
+app.get("/subcategories", (req, res) => {});
+
+app.get("/vendors", (req, res) => {});
+
+app.get("/vendorprofile", (req, res) => {});
+
+app.get("/vendorsignup", (req, res) => {});
+
 module.exports = app;
