c4e8d5fa97d1e3ce4b34dd97a9a8a8134575add0
shugao0624 <gaoshuyang0624@gmail.com>
2021-11-04 12:31:36 -0400

    created public folder for static files

--- back-end/app.js
+++ back-end/app.js
@@ -1,5 +1,5 @@
 // import and instantiate express
 const express = require("express"); // CommonJS import style!
 const app = express(); // instantiate an Express object
-
+app.use("/static", express.static("public"));
 module.exports = app;
