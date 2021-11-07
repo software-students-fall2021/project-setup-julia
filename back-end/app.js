066d8bd70d776a83d234040edf45febd3bbf8be6
shugao0624 <gaoshuyang0624@gmail.com>
2021-11-07 12:54:24 -0500

    added route handler to for user editing their profile

--- back-end/app.js
+++ back-end/app.js
@@ -1,33 +1,13 @@
 // import and instantiate express
 const express = require("express"); // CommonJS import style!
-const {
-  default: AvAddToQueue,
-} = require("material-ui/svg-icons/av/add-to-queue");
 const app = express(); // instantiate an Express object
+
 app.use("/static", express.static("public"));
+app.use(express.json()); // decode JSON-formatted incoming POST data
+app.use(express.urlencoded({ extended: true })); // decode url-encoded incoming POST data
 
-app.get("/home", (req, res) => {});
-
-app.get("/about", (req, res) => {});
-
-app.get("/contact", (req, res) => {});
-
-app.get("/login", (req, res) => {});
-
-app.get("/usersignup", (req, res) => {});
-
-app.get("/userprofile", (req, res) => {});
-
-app.get("/userfollowing", (req, res) => {});
-
-app.get("/searchresults", (req, res) => {});
-
-app.get("/subcategories", (req, res) => {});
-
-app.get("/vendors", (req, res) => {});
-
-app.get("/vendorprofile", (req, res) => {});
-
-app.get("/vendorsignup", (req, res) => {});
+app.get("/UserProfileForm", (req, res) => {
+  console.log(req.data);
+});
 
 module.exports = app;
