63b3de44058d385690703c419ef596c87bef2ac0
tazasahar <ms11831@nyu.edu>
2021-11-07 20:55:51 -0500

    added handleSumbit on my front end which I will use to post to my backend route. I still get an error on axios which I want to resolve tomorrow.

--- back-end/app.js
+++ back-end/app.js
@@ -1,30 +1,29 @@
 // import and instantiate express
-const express = require("express"); // CommonJS import style!
-const app = express(); // instantiate an Express object
-
-app.use("/static", express.static("public"));
-app.use(express.json()); // decode JSON-formatted incoming POST data
-app.use(express.urlencoded({ extended: true })); // decode url-encoded incoming POST data
-
-app.get("/UserProfileForm", (req, res) => {
-  console.log(req.data);
-});
-
 const express = require('express') // CommonJS import style!
-
-// const {
-//   default: AvAddToQueue,
-// } = require("material-ui/svg-icons/av/add-to-queue");
-
 const app = express() // instantiate an Express object
 
-const port = 5000
+app.use('/static', express.static('public'))
+app.use(express.json()) // decode JSON-formatted incoming POST data
+
+app.use(express.urlencoded({ extended: true })) // decode url-encoded incoming POST data
+
+app.get('/UserProfileForm', (req, res) => {
+  console.log(req.data)
+})
 
 app.get('/', (req, res) => res.send('hello world'))
 
-app.listen(port, () => console.log(`app listening on port ${port}!`))
+app.post('/userSignUp', (req, res) => {
+  //console.log(req.data)
+  const body = {
+    title: 'your message',
+    heading: 'bob is your dad',
+  }
+  res.json(body)
+})
+
+app.get('/userSignUp', (req, res) => res.send('hello world'))
 
 app.use('/static', express.static('public'))
-
 
 module.exports = app
